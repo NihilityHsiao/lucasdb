6688d19caa1db2d89e8c9a8649ae41f67c45c84a
NihilityHsiao <programmerHsiao@outlook.com>
2024-09-27 14:50:12 +0800

    improve: Add Sync + Send to Indexer trait

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -3,7 +3,7 @@ pub mod btree;
 use crate::data::log_record::LogRecordPos;
 
 /// 内存索引抽象接口
-pub trait Indexer {
+pub trait Indexer: Sync + Send {
     fn put(&self, key: Vec<u8>, pos: LogRecordPos) -> bool;
     fn get(&self, key: Vec<u8>) -> Option<LogRecordPos>;
     fn delete(&self, key: Vec<u8>) -> bool;
