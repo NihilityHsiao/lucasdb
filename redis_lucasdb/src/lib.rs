7fc0cd8f75a44c655f8a5248e7b6ca2466a4f362
NihilityHsiao <programmerHsiao@outlook.com>
2024-10-09 00:40:49 +0800

    🦄 refactor: 重构String操作

--- redis_lucasdb/src/lib.rs
+++ redis_lucasdb/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod generic;
+pub mod string;
 pub mod types;
--- redis_lucasdb/src/types.rs
+++ redis_lucasdb/src/types.rs
@@ -1,8 +1,6 @@
-use core::{fmt, time};
-use std::time::{SystemTime, UNIX_EPOCH};
+use core::fmt;
 
-use bytes::{Buf, BufMut, Bytes, BytesMut};
-use lucasdb::errors::{Errors, Result};
+use lucasdb::errors::Result;
 use lucasdb::options::EngineOptions;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -47,67 +45,5 @@ impl RedisLucasDb {
     pub fn new(options: EngineOptions) -> Result<Self> {
         let engine = lucasdb::db::Engine::open(options)?;
         Ok(Self { eng: engine })
-    }
-
-    /// value会经过编码再进行存储
-    /// 编码格式： type + ttl + value(用户传进的value)
-    pub fn set(&self, key: &str, ttl: std::time::Duration, value: &str) -> Result<()> {
-        if value.len() == 0 {
-            return Ok(());
-        }
-
-        let mut buf = BytesMut::new();
-        buf.put_u8(RedisDataType::String as u8); // 1.type
-
-        let mut expire = 0; // 过期时间,纳秒
-        if ttl != time::Duration::ZERO {
-            if let Some(v) = SystemTime::now().checked_add(ttl) {
-                expire = v.duration_since(UNIX_EPOCH).unwrap().as_nanos();
-            }
-        }
-
-        buf.put_u128(expire); // 2.ttl
-
-        // 3.value部分
-        buf.extend_from_slice(value.as_bytes());
-
-        self.eng
-            .put(Bytes::copy_from_slice(key.as_bytes()), buf.into())?;
-
-        Ok(())
-    }
-
-    // 拿到的value需要解码
-    /// 编码格式： type + ttl + value(用户传进的value)
-    pub fn get(&self, key: &str) -> Result<Option<String>> {
-        let mut buf = self.eng.get(Bytes::copy_from_slice(key.as_bytes()))?;
-        let key_type = RedisDataType::from(buf.get_u8());
-
-        // 判断key的类型能否执行get操作
-        if key_type != RedisDataType::String {
-            return Err(Errors::WrongTypeOperation {
-                expected: RedisDataType::String.to_string(),
-                actual: key_type.to_string(),
-            });
-        }
-
-        // 判断过期时间
-        let expire = buf.get_u128();
-        if expire > 0 {
-            let now = SystemTime::now()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap()
-                .as_nanos();
-            if expire <= now {
-                // 过期了
-                return Ok(None);
-            }
-        }
-
-        // 取出真正的value
-        // get_u8和get_u128会移动ptr位置,所以直接to_vec就得到value了
-        let value = buf.to_vec();
-
-        Ok(Some(String::from_utf8(value).unwrap()))
     }
 }
