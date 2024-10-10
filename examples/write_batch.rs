bb2131a9f5af58a6a1a66aea671e1366efd2cacb
NihilityHsiao <programmerHsiao@outlook.com>
2024-10-10 20:15:46 +0800

    🌈 style: 增加注释

--- examples/write_batch.rs
+++ examples/write_batch.rs
@@ -16,21 +16,24 @@ fn main() {
 
     let wb_opts = WriteBatchOptions::default();
 
+    // 打开一个数据库
     let db = Engine::open(db_opts.clone()).expect("failed to open database");
+    // 创建一个批处理
     let wb = db
         .new_write_batch(wb_opts.clone())
         .expect("failed to create write batch");
 
+    // 批量写入
     let _ = wb.put(Bytes::from("key-1"), Bytes::from("value-1"));
     let _ = wb.put(Bytes::from("key-2"), Bytes::from("value-2"));
     let _ = wb.put(Bytes::from("key-3"), Bytes::from("value-3"));
+    // 提交数据
     wb.commit().expect("failed to commit write batch");
 
     // 关闭
     db.close().expect("failed to close database");
 
-    // 打开一个新的
-
+    // 打开一个新的db实例
     let db = Engine::open(db_opts.clone()).expect("failed to open database 2");
 
     let keys = db.list_keys().unwrap();
